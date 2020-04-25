2b5b8d302094926c631fc14cf60b5dcc0a9074cd
kapehh-karen <mail@kapehh.net>
2020-04-26 01:45:28 +0300

    Add ApiController

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AkkaTestApi.Controllers
 {
+    [ApiController]
+    [Route("[controller]/[action]")]
     public class WeatherController : Controller
     {
         private readonly WeatherService _weatherService;
@@ -17,16 +19,12 @@ namespace AkkaTestApi.Controllers
 
         public IActionResult Create([Required] string cityName)
         {
-            if (!ModelState.IsValid) return BadRequest();
-
             _weatherService.CreateCity(cityName);
             return Ok("Ok");
         }
 
         public IActionResult UpdateWeather([Required] string cityName, [Required] double weather)
         {
-            if (!ModelState.IsValid) return BadRequest();
-
             _weatherService.UpdateWeather(cityName, weather);
             return Ok("Ok");
         }
@@ -39,8 +37,6 @@ namespace AkkaTestApi.Controllers
 
         public async Task<IActionResult> GetWeather([Required] string cityName)
         {
-            if (!ModelState.IsValid) return BadRequest();
-
             return Ok(new
             {
                 City = cityName,
